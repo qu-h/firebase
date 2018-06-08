370354e0b186e5420f5c42df57710e60f7052664
quanict <hongquan2712@gmail.com>
2018-06-08 22:37:57 +0700

    update

---
+++ command.md
@@ -0,0 +1,35 @@
+# Install
+```
+npm install -g firebase-tools
+```
+
+## install latest version
+
+```
+npm install firebase-functions@latest firebase-admin@latest --save
+```
+## Login 
+```
+firebase login 
+```
+
+### install functions
+```apple js
+firebase init functions
+```
+
+###To Run 
+```
+firebase serve --only functions
+firebase deploy --only functions:addMessage
+
+
+``` 
+
+###To deploy 
+```
+firebase deploy --only functions
+firebase deploy --only functions:addMessage
+
+
+``` 
--- functions/index.js
+++ functions/index.js
@@ -11,28 +11,66 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-        return res.redirect(303, snapshot.ref.toString());
-    });
+const translate = require('@google-cloud/translate')();
+// List of output languages.
+const LANGUAGES = ['en', 'es', 'de', 'fr', 'sv', 'ga', 'it', 'jp'];
+
+// Translate an incoming message.
+exports.translate = functions.database.ref('/messages/{languageID}/{messageID}').onWrite((change, context) => {
+    const snapshot = change.after;
+    if (snapshot.val().translated) {
+        return null;
+    }
+    const promises = [];
+    for (let i = 0; i < LANGUAGES.length; i++) {
+        const language = LANGUAGES[i];
+        if (language !== context.params.languageID) {
+            promises.push(translate.translate(snapshot.val().message, {from: context.params.languageID, to: language}).then(
+                (results) => {
+                    return admin.database().ref(`/messages/${language}/${snapshot.key}`).set({
+                        message: results[0],
+                        translated: true,
+                    });
+                }));
+        }
+    }
+    return Promise.all(promises);
 });
 
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-    .onCreate((snapshot, context) => {
-        // Grab the current value of what was written to the Realtime Database.
-        const original = snapshot.val();
-        console.log('Uppercasing', context.params.pushId, original);
-        const uppercase = original.toUpperCase();
-        // You must return a Promise when performing asynchronous tasks inside a Functions such as
-        // writing to the Firebase Realtime Database.
-        // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-        return snapshot.ref.parent.child('uppercase').set(uppercase);
+
+// Since this code will be running in the Cloud Functions enviornment
+// we call initialize Firestore without any arguments because it
+// detects authentication from the environment.
+const firestore = admin.firestore();
+
+// Create a new function which is triggered on changes to /status/{uid}
+// Note: This is a Realtime Database trigger, *not* Cloud Firestore.
+exports.onUserStatusChanged = functions.database.ref('/status/{uid}').onUpdate(
+    (change, context) => {
+        // Get the data written to Realtime Database
+        const eventStatus = change.after.val();
+
+        // Then use other event data to create a reference to the
+        // corresponding Firestore document.
+        const userStatusFirestoreRef = firestore.doc(`status/${context.params.uid}`);
+
+        // It is likely that the Realtime Database change that triggered
+        // this event has already been overwritten by a fast change in
+        // online / offline status, so we'll re-read the current data
+        // and compare the timestamps.
+        return change.after.ref.once('value').then((statusSnapshot) => {
+            const status = statusSnapshot.val();
+            console.log(status, eventStatus);
+            // If the current timestamp for this data is newer than
+            // the data that triggered this event, we exit this function.
+            if (status.last_changed > eventStatus.last_changed) {
+                return null;
+            }
+
+            // Otherwise, we convert the last_changed field to a Date
+            eventStatus.last_changed = new Date(eventStatus.last_changed);
+
+            // ... and write it to Firestore.
+            return userStatusFirestoreRef.set(eventStatus);
+        });
     });
